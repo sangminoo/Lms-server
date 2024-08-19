96d57b652419689cee4905438f62829f3f759dc0
Sang Mino <minosang1712@gmail.com>
2024-08-19 13:59:41 +0700

    done

--- app.ts
+++ app.ts
@@ -23,7 +23,7 @@ app.use(cookieParser());
 // cors => cross origin resource sharing
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://lms-client-kappa.vercel.app/"],
     credentials: true,
   })
 );
